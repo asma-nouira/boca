439d25c52bc46479427ce1cc87df8053bf1e7427
asma <asma@viruscomm.ca>
2026-08-17 09:43:37 +0100

    home page

--- wp-content/themes/vsc-theme/header.php
+++ wp-content/themes/vsc-theme/header.php
@@ -109,10 +109,24 @@
 						?>
 						<div class="menu-overlay__social">
 						<a href="https://facebook.com/" target="_blank" rel="noopener" aria-label="Facebook">
-							<svg width="16" height="16" viewBox="0 0 24 24" fill="none" aria-hidden="true"><circle cx="12" cy="12" r="10.5" stroke="currentColor" stroke-width="1.3"/><path d="M13.8 8.6h1.4V6.3h-1.7c-1.8 0-2.9 1.1-2.9 3v1.4H9.1v2.1h1.5v6.1h2.3v-6.1h1.6l.3-2.1h-1.9V9.5c0-.6.3-.9.9-.9z" fill="currentColor"/></svg>
+						<svg xmlns="http://www.w3.org/2000/svg" width="57.372" height="57.372" viewBox="0 0 57.372 57.372">
+  <g id="Groupe_247" data-name="Groupe 247" transform="translate(-604 -8595.5)">
+    <path id="Tracé_430" data-name="Tracé 430" d="M770.022,46.813h-6.865V29.959h-5.611V23.413h5.585c.013-.276.03-.492.034-.708.032-1.823-.022-3.652.108-5.468a7.578,7.578,0,0,1,7.664-7.351c1.737-.051,3.48.086,5.219.146a4.1,4.1,0,0,1,.582.108v5.875c-.7,0-1.348-.015-1.993,0-.9.025-1.8.015-2.694.127a2,2,0,0,0-1.918,1.923c-.1,1.749-.026,3.506-.026,5.323h6.413l-.845,6.563h-5.653Z" transform="translate(-134.429 8596)" fill="#43544a"/>
+    <circle id="Ellipse_2" data-name="Ellipse 2" cx="27.686" cy="27.686" r="27.686" transform="translate(605 8596.5)" fill="none" stroke="#43544a" stroke-miterlimit="10" stroke-width="2"/>
+  </g>
+</svg>
+
 						</a>
 						<a href="https://instagram.com/" target="_blank" rel="noopener" aria-label="Instagram">
-							<svg width="16" height="16" viewBox="0 0 24 24" fill="none" aria-hidden="true"><rect x="3" y="3" width="18" height="18" rx="5" stroke="currentColor" stroke-width="1.3"/><circle cx="12" cy="12" r="4" stroke="currentColor" stroke-width="1.3"/><circle cx="17.2" cy="6.8" r="1" fill="currentColor"/></svg>
+							<svg xmlns="http://www.w3.org/2000/svg" width="57.372" height="57.372" viewBox="0 0 57.372 57.372">
+  <g id="Groupe_248" data-name="Groupe 248" transform="translate(-604 -8675.928)">
+    <circle id="Ellipse_3" data-name="Ellipse 3" cx="27.686" cy="27.686" r="27.686" transform="translate(605 8676.928)" fill="none" stroke="#43544a" stroke-miterlimit="10" stroke-width="2"/>
+    <path id="Tracé_431" data-name="Tracé 431" d="M767.042,123.574c-2.714-.115-5.386-.122-8.036-.365a7.889,7.889,0,0,1-7.318-7.662c-.149-4.944-.205-9.9,0-14.842a7.833,7.833,0,0,1,7.645-7.689c4.206-.243,8.43-.178,12.646-.177a19.447,19.447,0,0,1,4.048.381,7.812,7.812,0,0,1,6.33,7.452c.166,5.025.172,10.066-.018,15.089a7.842,7.842,0,0,1-7.278,7.442c-2.676.25-5.375.257-8.019.371M766.987,95.4c0,.076,0,.152-.005.228-2.231,0-4.464-.059-6.692.017a5.667,5.667,0,0,0-5.04,2.65,7.635,7.635,0,0,0-.934,4c-.024,4.027-.066,8.057.02,12.083.086,3.991,2.253,6.146,6.233,6.209,4.432.069,8.869.052,13.3-.008,3.164-.043,5.6-2.105,5.69-5.055.141-4.917.125-9.843,0-14.761a5.06,5.06,0,0,0-4.675-4.957c-2.619-.269-5.264-.283-7.9-.409" transform="translate(-134.429 8596)" fill="#43544a"/>
+    <path id="Tracé_432" data-name="Tracé 432" d="M759.019,108.044a8.009,8.009,0,1,1,7.915,7.982,7.973,7.973,0,0,1-7.915-7.982m7.995,5.2a5.189,5.189,0,0,0,5.222-5.072,5.211,5.211,0,1,0-10.421.072,5.18,5.18,0,0,0,5.2,5" transform="translate(-134.429 8596)" fill="#43544a"/>
+    <path id="Tracé_433" data-name="Tracé 433" d="M777.173,99.9a1.84,1.84,0,1,1-3.68.011,1.84,1.84,0,0,1,3.68-.011" transform="translate(-134.429 8596)" fill="#ecd8c9"/>
+  </g>
+</svg>
+
 						</a>
 					</div>
 
