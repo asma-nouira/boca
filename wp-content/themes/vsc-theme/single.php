1ee2810fc6fafa00ee26289e7ff309994c4f819d
asma <asma@viruscomm.ca>
2026-09-03 11:08:17 +0100

    page blogue

--- wp-content/themes/vsc-theme/single.php
+++ wp-content/themes/vsc-theme/single.php
@@ -13,7 +13,26 @@ get_header();
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
+<div class="vce-row-container" data-vce-boxed-width="true"><div class="vce-row vce-element--has-background vce-row--col-gap-30 vce-row-equal-height vce-row-content--top" id="el-d21af246" data-vce-do-apply="all el-d21af246"><div class="vce-content-background-container"><div class="vce-asset-background-simple-container vce-visible-all-only vce-asset-background-simple--style-cover vce-asset-background-simple--position-center-center"><div class="vce-asset-background-simple">
+              <div class="vce-asset-background-simple-item vcv-lozad" data-background-image="/wp-content/uploads/2026/09/Blogue.jpg" data-loaded="true" style="background-image: url(&quot;/wp-content/uploads/2026/09/Blogue.jpg&quot;);"></div>
+              <noscript>
+                <div class="vce-asset-background-simple-item" style="background-image: url(/wp-content/uploads/2026/09/Blogue.jpg)"></div>
+              </noscript>
+            </div></div></div><div class="vce-row-content" data-vce-element-content="true"><div class="vce-col vce-col--md-auto vce-col--xs-1 vce-col--xs-last vce-col--xs-first vce-col--sm-last vce-col--sm-first vce-col--md-last vce-col--lg-last vce-col--xl-last vce-col--md-first vce-col--lg-first vce-col--xl-first hero-section-page faq" id="el-a29d459d"><div class="vce-col-inner" data-vce-do-apply="border margin background  el-a29d459d"><div class="vce-col-content" data-vce-element-content="true" data-vce-do-apply="padding el-a29d459d"><div class="vce-text-block pb-30 creme reveal-up reveal-delay-1 in-view"><div class="vce-text-block-wrapper vce" id="el-e92a95a2" data-vce-do-apply="all el-e92a95a2"><p class="section-title hero-fade-in" style="animation-delay: 0.25s;">Blogue</p>
+<h1 class="hero-fade-in" style="animation-delay: 0.1s;">Blogue</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec urna felis, eleifend in tincidunt ac, rhoncus eu eros. Vestibulum tempus velit magna, ut tincidunt leo ultricies finibus. Mauris congue libero ut elit vehicula</p>
+</div></div><div class="vce-global-element vce reveal-up reveal-delay-2 in-view" id="el-ec740af0" data-vce-do-apply="all el-ec740af0"><div class="vce-global-template-inner"><style id="vcv:assets:front:style:168-inline-css">
+.vce{margin-bottom:30px}.vce-row-container{width:100%}.vce-row{display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;margin-left:0;position:relative;transition:box-shadow .2s}.vce-row,.vce-row-content>.vce-col:last-child{margin-right:0}.vce-row-full-height{min-height:100vh}.vce-row-content{-ms-flex-pack:start;-ms-flex-line-pack:start;-ms-flex-align:start;align-content:flex-start;align-items:flex-start;display:-ms-flexbox;display:flex;-ms-flex:1 1 auto;flex:1 1 auto;-ms-flex-direction:row;flex-direction:row;-ms-flex-wrap:wrap;flex-wrap:wrap;justify-content:flex-start;min-height:1em;position:relative}.vce-row-wrap--reverse>.vce-row-content{-ms-flex-line-pack:end;-ms-flex-align:end;align-content:flex-end;align-items:flex-end;-ms-flex-wrap:wrap-reverse;flex-wrap:wrap-reverse}.vce-row-columns--top>.vce-row-content{-ms-flex-line-pack:start;align-content:flex-start}.vce-row-columns--top.vce-row-wrap--reverse>.vce-row-content{-ms-flex-line-pack:end;align-content:flex-end}.vce-row-columns--middle>.vce-row-content{-ms-flex-line-pack:center;align-content:center}.vce-row-columns--bottom>.vce-row-content{-ms-flex-line-pack:end;align-content:flex-end}.vce-row-columns--bottom.vce-row-wrap--reverse>.vce-row-content{-ms-flex-line-pack:start;align-content:flex-start}.vce-row-columns--bottom>.vce-row-content:after,.vce-row-columns--middle>.vce-row-content:after,.vce-row-columns--top>.vce-row-content:after{content:"";display:block;height:0;overflow:hidden;visibility:hidden;width:100%}.vce-row-content--middle>.vce-row-content>.vce-col>.vce-col-inner{-ms-flex-pack:center;display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;justify-content:center}.vce-row-content--bottom>.vce-row-content>.vce-col>.vce-col-inner{-ms-flex-pack:end;display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;justify-content:flex-end}.vce-row-equal-height>.vce-row-content{-ms-flex-align:stretch;align-items:stretch}.vce-row-columns--stretch>.vce-row-content{-ms-flex-line-pack:stretch;-ms-flex-align:stretch;align-content:stretch;align-items:stretch}.vce-row[data-vce-full-width=true]{box-sizing:border-box;position:relative}.vce-row[data-vce-stretch-content=true]{padding-left:30px;padding-right:30px}.vce-row[data-vce-stretch-content=true].vce-row-no-paddings{padding-left:0;padding-right:0}.vce-row.vce-element--has-background{padding-left:30px;padding-right:30px;padding-top:30px}.vce-row.vce-element--has-background[data-vce-full-width=true]:not([data-vce-stretch-content=true]){padding-left:0;padding-right:0}.vce-row.vce-element--has-background.vce-row--has-col-background{padding-bottom:30px}.vce-row>.vce-row-content>.vce-col.vce-col--all-last{margin-right:0}.rtl .vce-row>.vce-row-content>.vce-col.vce-col--all-last,.rtl.vce-row>.vce-row-content>.vce-col.vce-col--all-last{margin-left:0}@media (min-width:0) and (max-width:543px){.vce-row.vce-element--xs--has-background{padding-left:30px;padding-right:30px;padding-top:30px}.vce-row.vce-element--xs--has-background[data-vce-full-width=true]:not([data-vce-stretch-content=true]){padding-left:0;padding-right:0}.vce-row.vce-element--has-background.vce-row--xs--has-col-background,.vce-row.vce-element--xs--has-background.vce-row--has-col-background,.vce-row.vce-element--xs--has-background.vce-row--xs--has-col-background{padding-bottom:30px}.vce-row>.vce-row-content>.vce-col.vce-col--xs-last{margin-right:0}.rtl .vce-row>.vce-row-content>.vce-col.vce-col--xs-last,.rtl.vce-row>.vce-row-content>.vce-col.vce-col--xs-last{margin-left:0}}@media (min-width:544px) and (max-width:767px){.vce-row.vce-element--sm--has-background{padding-left:30px;padding-right:30px;padding-top:30px}.vce-row.vce-element--sm--has-background[data-vce-full-width=true]:not([data-vce-stretch-content=true]){padding-left:0;padding-right:0}.vce-row.vce-element--has-background.vce-row--sm--has-col-background,.vce-row.vce-element--sm--has-background.vce-row--has-col-background,.vce-row.vce-element--sm--has-background.vce-row--sm--has-col-background{padding-bottom:30px}.vce-row>.vce-row-content>.vce-col.vce-col--sm-last{margin-right:0}.rtl .vce-row>.vce-row-content>.vce-col.vce-col--sm-last,.rtl.vce-row>.vce-row-content>.vce-col.vce-col--sm-last{margin-left:0}}@media (min-width:768px) and (max-width:991px){.vce-row.vce-element--md--has-background{padding-left:30px;padding-right:30px;padding-top:30px}.vce-row.vce-element--md--has-background[data-vce-full-width=true]:not([data-vce-stretch-content=true]){padding-left:0;padding-right:0}.vce-row.vce-element--has-background.vce-row--md--has-col-background,.vce-row.vce-element--md--has-background.vce-row--has-col-background,.vce-row.vce-element--md--has-background.vce-row--md--has-col-background{padding-bottom:30px}.vce-row>.vce-row-content>.vce-col.vce-col--md-last{margin-right:0}.rtl .vce-row>.vce-row-content>.vce-col.vce-col--md-last,.rtl.vce-row>.vce-row-content>.vce-col.vce-col--md-last{margin-left:0}}@media (min-width:992px) and (max-width:1199px){.vce-row.vce-element--lg--has-background{padding-left:30px;padding-right:30px;padding-top:30px}.vce-row.vce-element--lg--has-background[data-vce-full-width=true]:not([data-vce-stretch-content=true]){padding-left:0;padding-right:0}.vce-row.vce-element--has-background.vce-row--lg--has-col-background,.vce-row.vce-element--lg--has-background.vce-row--has-col-background,.vce-row.vce-element--lg--has-background.vce-row--lg--has-col-background{padding-bottom:30px}.vce-row>.vce-row-content>.vce-col.vce-col--lg-last{margin-right:0}.rtl .vce-row>.vce-row-content>.vce-col.vce-col--lg-last,.rtl.vce-row>.vce-row-content>.vce-col.vce-col--lg-last{margin-left:0}}@media (min-width:1200px){.vce-row.vce-element--xl--has-background{padding-left:30px;padding-right:30px;padding-top:30px}.vce-row.vce-element--xl--has-background[data-vce-full-width=true]:not([data-vce-stretch-content=true]){padding-left:0;padding-right:0}.vce-row.vce-element--has-background.vce-row--xl--has-col-background,.vce-row.vce-element--xl--has-background.vce-row--has-col-background,.vce-row.vce-element--xl--has-background.vce-row--xl--has-col-background{padding-bottom:30px}.vce-row>.vce-row-content>.vce-col.vce-col--xl-last{margin-right:0}.rtl .vce-row>.vce-row-content>.vce-col.vce-col--xl-last,.rtl.vce-row>.vce-row-content>.vce-col.vce-col--xl-last{margin-left:0}}.vce-col{box-sizing:border-box;display:-ms-flexbox;display:flex;-ms-flex:0 0 100%;flex:0 0 100%;max-width:100%;min-width:1em;position:relative}.vce-col--auto{-ms-flex-preferred-size:auto;-ms-flex:1;flex:1;flex-basis:auto}.vce-col-content,.vce-col-inner{overflow-wrap:break-word;position:relative;transition:box-shadow .2s}.vce-col-inner{width:100%}.vce-col-direction--rtl{direction:rtl}.vce-element--has-background>.vce-col-inner>.vce-col-content{padding-left:30px;padding-right:30px;padding-top:30px}.vce-col.vce-col--all-hide{display:none}@media (min-width:0),print{.vce-col--xs-auto{-ms-flex:1 1 0;flex:1 1 0;width:auto}.vce-col--xs-1{-ms-flex:0 0 100%;flex:0 0 100%;max-width:100%}.vce-col.vce-col--xs-visible{display:-ms-flexbox;display:flex}.vce-col.vce-col--xs-hide{display:none}}.vce-row-container>.vce-row>.vce-row-content>.vce-col>.vce-col-inner{padding:0}@media (min-width:544px),print{.vce-col--sm-auto{-ms-flex:1 1 0;flex:1 1 0;width:auto}.vce-col--sm-1{-ms-flex:0 0 100%;flex:0 0 100%;max-width:100%}.vce-col.vce-col--sm-visible{display:-ms-flexbox;display:flex}.vce-col.vce-col--sm-hide{display:none}}@media (min-width:768px),print{.vce-col--md-auto{-ms-flex:1 1 0;flex:1 1 0;width:auto}.vce-col--md-1{-ms-flex:0 0 100%;flex:0 0 100%;max-width:100%}.vce-col.vce-col--md-visible{display:-ms-flexbox;display:flex}.vce-col.vce-col--md-hide{display:none}}@media print,screen and (min-width:992px){.vce-col--lg-auto{-ms-flex:1 1 0;flex:1 1 0;width:1px}.vce-col--lg-1{-ms-flex:0 0 100%;flex:0 0 100%;max-width:100%}.vce-col.vce-col--lg-visible{display:-ms-flexbox;display:flex}.vce-col.vce-col--lg-hide{display:none}}@media (min-width:1200px),print{.vce-col--xl-auto{-ms-flex:1 1 0;flex:1 1 0;width:auto}.vce-col--xl-1{-ms-flex:0 0 100%;flex:0 0 100%;max-width:100%}.vce-col.vce-col--xl-visible{display:-ms-flexbox;display:flex}.vce-col.vce-col--xl-hide{display:none}}@media (min-width:0) and (max-width:543px){.vce-element--xs--has-background>.vce-col-inner>.vce-col-content{padding-left:30px;padding-right:30px;padding-top:30px}}@media (min-width:544px) and (max-width:767px){.vce-element--sm--has-background>.vce-col-inner>.vce-col-content{padding-left:30px;padding-right:30px;padding-top:30px}}@media (min-width:768px) and (max-width:991px){.vce-element--md--has-background>.vce-col-inner>.vce-col-content{padding-left:30px;padding-right:30px;padding-top:30px}}@media (min-width:992px) and (max-width:1199px){.vce-element--lg--has-background>.vce-col-inner>.vce-col-content{padding-left:30px;padding-right:30px;padding-top:30px}}@media (min-width:1200px){.vce-element--xl--has-background>.vce-col-inner>.vce-col-content{padding-left:30px;padding-right:30px;padding-top:30px}}.vce-row--col-gap-30>.vce-row-content>.vce-col{margin-right:30px}.vce-row--col-gap-30>.vce-row-content>.vce-column-resizer .vce-column-resizer-handler{width:30px}.rtl .vce-row--col-gap-30>.vce-row-content>.vce-col,.rtl.vce-row--col-gap-30>.vce-row-content>.vce-col{margin-left:30px;margin-right:0}@media all and (min-width:768px),print{.vce-row--col-gap-30[data-vce-do-apply*="7570b242"]>.vce-row-content>.vce-col--md-auto{-webkit-flex:1 1 0;flex:1 1 0;max-width:none;width:auto}}
+/*# sourceURL=vcv%3Aassets%3Afront%3Astyle%3A168-inline-css */
+</style>
+<!--vcv no format--><div class="vce-row-container" data-vce-boxed-width="true"><div class="vce-row vce-row--col-gap-30 vce-row-equal-height vce-row-content--top" id="el-7570b242" data-vce-do-apply="all el-7570b242"><div class="vce-row-content" data-vce-element-content="true"><div class="vce-col vce-col--md-auto vce-col--xs-1 vce-col--xs-last vce-col--xs-first vce-col--sm-last vce-col--sm-first vce-col--md-last vce-col--lg-last vce-col--xl-last vce-col--md-first vce-col--lg-first vce-col--xl-first" id="el-9f6819be"><div class="vce-col-inner" data-vce-do-apply="border margin background  el-9f6819be"><div class="vce-col-content" data-vce-element-content="true" data-vce-do-apply="padding el-9f6819be"><div class="vce-text-block"><div class="vce-text-block-wrapper vce" id="el-5807f912" data-vce-do-apply="all el-5807f912"><p><a class="btn-appointment hero-fade-in" href="https://www.docclik.com/fr/clinic/12333/booking" target="_blank" rel="noopener" style="animation-delay: 0.4s;">Prendre rendez-vous</a></p>
+</div></div></div></div></div></div></div></div><!--vcv no format--></div></div></div></div></div></div></div></div>
 
+<div class="vce-row-container" data-vce-boxed-width="true"><div class="vce-row vce-row--col-gap-30 fil-ariane vce-row-equal-height vce-row-content--top" id="el-10640325" data-vce-do-apply="all el-10640325"><div class="vce-row-content" data-vce-element-content="true"><div class="vce-col vce-col--md-auto vce-col--xs-1 vce-col--xs-last vce-col--xs-first vce-col--sm-last vce-col--sm-first vce-col--md-last vce-col--lg-last vce-col--xl-last vce-col--md-first vce-col--lg-first vce-col--xl-first" id="el-01e76d24"><div class="vce-col-inner" data-vce-do-apply="border margin background  el-01e76d24"><div class="vce-col-content" data-vce-element-content="true" data-vce-do-apply="padding el-01e76d24"><div class="vce-shortcode"><div class="vce-shortcode-wrapper vce" id="el-1fc08a03" data-vce-do-apply="all el-1fc08a03"><span><span><a href="/">Accueil</a></span> | <span class="breadcrumb_last" aria-current="page">Blogue</span></span></div></div></div></div></div></div></div></div>
+	<div class="vce-row-container" data-vce-boxed-width="true"><div class="vce-row vce-row--col-gap-30 plr-416 pt-90 pb-130 vce-row-equal-height vce-row-content--top" id="el-71d10ae9" data-vce-do-apply="all el-71d10ae9"><div class="vce-row-content" data-vce-element-content="true"><div class="vce-col vce-col--md-auto vce-col--xs-1 vce-col--xs-last vce-col--xs-first vce-col--sm-last vce-col--sm-first vce-col--md-last vce-col--lg-last vce-col--xl-last vce-col--md-first vce-col--lg-first vce-col--xl-first" id="el-6acb3ff5"><div class="vce-col-inner" data-vce-do-apply="border margin background  el-6acb3ff5"><div class="vce-col-content" data-vce-element-content="true" data-vce-do-apply="padding el-6acb3ff5"><div class="vce-text-block center reveal-up reveal-delay-1"><div class="vce-text-block-wrapper vce" id="el-4d4bba1d" data-vce-do-apply="all el-4d4bba1d"><h2 class="pb-70 reveal-up reveal-delay-2">Blogue</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec urna felis, eleifend in tincidunt ac, rhoncus eu eros. Vestibulum tempus velit magna, ut tincidunt leo ultricies finibus. Mauris congue libero ut elit vehicula, non condimentum tellus finibus. Morbi rhoncus lacus velit, eget vehicula nisi pellentesque sit amet. Mauris malesuada semper molestie. Nulla facilisi.</p>
+</div></div></div></div></div></div></div></div>
+	
 	<?php
 	while ( have_posts() ) :
 		the_post();
@@ -67,6 +86,119 @@ get_header();
 
 	</main><!-- #main -->
 </div><!-- #primary -->
-
+<style>
+	.vce-row-container {
+    width: 100%;
+}
+.vce-row {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    margin-left: 0;
+    position: relative;
+    transition: box-shadow .2s;
+}
+.vce-asset-background-simple-container.vce-visible-all-only {
+    display: block;
+}
+.vce-asset-background-simple-container {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+    pointer-events: none;
+    display: none;
+    border-radius: inherit;
+}
+.vce-asset-background-simple {
+    position: absolute;
+    top: 0;
+    right: -1px;
+    bottom: 0;
+    left: -1px;
+}
+.vce-row.vce-element--has-background:first-of-type .vce-asset-background-simple-item, main .vce-row:has(h1) .vce-asset-background-simple-item {
+    animation: bocaKenBurns 18s ease-in-out infinite alternate;
+}
+.vce-asset-background-simple--position-center-center .vce-asset-background-simple-item {
+    background-position: 50%;
+}
+.vce-asset-background-simple--style-cover .vce-asset-background-simple-item {
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+.vce-asset-background-simple-item {
+    background-position: 50% 0;
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+.vce-row-equal-height>.vce-row-content {
+    -ms-flex-align: stretch;
+    align-items: stretch;
+}
+.vce-row-content {
+    -ms-flex-pack: start;
+    -ms-flex-line-pack: start;
+    -ms-flex-align: start;
+    align-content: flex-start;
+    align-items: flex-start;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    min-height: 1em;
+    position: relative;
+}
+.vce-col-content, .vce-col-inner {
+    overflow-wrap: break-word;
+    position: relative;
+    transition: box-shadow .2s;
+}
+@media (min-width: 1200px) {
+    .vce-row>.vce-row-content>.vce-col.vce-col--xl-last {
+        margin-right: 0;
+    }
+}
+@media (min-width: 768px), print {
+    .vce-row--col-gap-30[data-vce-do-apply*="10640325"]>.vce-row-content>.vce-col--md-auto {
+        -webkit-flex: 1 1 0;
+        flex: 1 1 0;
+        max-width: none;
+        width: auto;
+    }
+}
+.vce-row>.vce-row-content>.vce-col.vce-col--xl-last {
+    margin-right: 0;
+}
+.vce-col--md-auto {
+    -ms-flex: 1 1 0;
+    flex: 1 1 0;
+    width: auto;
+}
+@media (min-width: 0), print {
+    .vce-col--xs-1 {
+        -ms-flex: 0 0 100%;
+        flex: 0 0 100%;
+        max-width: 100%;
+    }
+}
+.vce-col--md-auto {
+    -ms-flex: 1 1 0;
+    flex: 1 1 0;
+    width: auto;
+}
+</style>
 <?php
 get_footer();
